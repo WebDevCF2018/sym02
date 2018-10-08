d99156fc04a2cf070e77f4a53c3926144f374d83
mikhawa <michaeljpitz@gmail.com>
2018-10-08 16:02:47 +0200

    change routing,  controller and views

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+// using entity Article
+use App\Entity\Article;
 
 class AccueilController extends AbstractController
 {
@@ -12,7 +14,19 @@ class AccueilController extends AbstractController
      */
     public function accueil()
     {
-        return $this->render('accueil/index.html.twig', [
+        // create a Doctrine manager and get back all articles
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
+
+        // create view with twig, with attribute NosArticles
+        return $this->render("accueil/accueil.html.twig", ["NosArticles" => $articles]);
+    }
+
+    /**
+     * @Route("/debug/AccueilController", name="debugAccueilController")
+     */
+    public function debug()
+    {
+        return $this->render('accueil/debug.html.twig', [
             'controller_name' => 'AccueilController',
         ]);
     }
